085ceeb8a793031419f84fd9bdbeb3f6ce9a329b
Nathan Shepherd <nateshep@localhost.localdomain>
2019-11-14 14:06:33 -0500

    Completed

--- decoder.py
+++ decoder.py
@@ -1,5 +1,4 @@
 import csv
-import numpy as np
 
 msg = []
 with open('message.csv') as csv_file:
@@ -8,7 +7,6 @@ with open('message.csv') as csv_file:
     for row in csv_reader:
     	msg.append([int(num) for num in row])
 
-msg = np.array(msg)
 
 gBig =  '''1 0 0 0 0
 0 1 0 0 0
@@ -20,39 +18,67 @@ gBig =  '''1 0 0 0 0
 1 0 1 1 0
 1 0 0 1 1'''
 
-g = []
-for elem in gBig.split("\n"):
-	g.append([int(num) for num in elem.split(" ")])
+g = gBig.replace(" ", "").split("\n")
 
-g = np.array(g)
+'''
+Convert alphabet to binary representation
+Associate conversion in dictionary {binary: letter}
+Encode each letter using gBig
+Compare encrypted message to encrypted alphabet
+Recover letter codes in message changed during transmission
+  (msg letter codes differ from encypted letters by <= 1 bit)
+'''
 
-#print(np.dot(g, [0, 0, 0, 0, 1]))
-alpha_orig = np.array([bin(num)[2:].ljust(5, '0') for num in range(27)])
+
+alpha_orig = [bin(num).replace("0b","").rjust(5, '0') for num in range(27)]
+#print(alpha_orig)
 
 letters = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split(" ")
+letters = [" "] + letters #space is a letter
 alpha = {}
-for i, l in enumerate(letters):
-	arr = []
-	for j in range(len(alpha_orig[i])):
-		arr.append(int(alpha_orig[i][j]))
+
+def dot(lhs, rhs):
+	result = ""
+	for row in lhs:
+		found_one = False
+		print(row, rhs)
+		summ = 0
+		for i in range(len(row)):
+			if row[i] == '1' and rhs[i] == '1':
+				summ += 1
+		result += str(summ % 2) #summ to binary
+
+	print("dot_prod(gBig,",rhs +") =", result)
+	return result
+
+#Encode each letter using gBig
+prod = []
+for i in range(len(alpha_orig)):
+	prod.append( dot(g, alpha_orig[i]))
 	
-	######## TODO: this dot product in binary
-	encoded = np.dot(g, np.array(arr))
-	alpha[str(encoded)] = l
 
-print(alpha)
+#Associate alphabet encoding to each letter
+alpha = dict(zip(prod, letters))
 
-decoded = ""
-print(msg)
+print('\n',len(alpha), alpha)
+
+decoded = "" #;print(msg)
+
 for char in msg:
-	char = str(char)	
+	#cast char into binary string from array representation
+	char = str(char)[1:-1].replace(", ", "")
+
 	for i in range(len(char)):
 		temp = char
-		if temp[i] == '0':
-			temp = temp[:i]+ '1' +temp[i + 1:]
-		else:
-			temp = temp[:i]+ '0' +temp[i + 1:]
+		if temp not in alpha:					
+			if temp[i] == '0':
+				temp = temp[:i]+ '1' +temp[i + 1:]
+			else:
+				temp = temp[:i]+ '0' +temp[i + 1:]
+		
 		if temp in alpha:
 			decoded += alpha[temp]
-print(decoded)
+			break
+
+print("\nDecoded message with len",len(decoded),":", decoded)
 
