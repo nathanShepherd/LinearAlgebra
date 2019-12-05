063e050d785e2be3251ac27ff17976f4f2ec6aed
Nathan Shepherd <nateshep@0587362664.wireless.umich.net>
2019-12-05 16:58:40 -0500

    Updates

--- DataCompression/SVD.py
+++ DataCompression/SVD.py
@@ -7,6 +7,33 @@
 # V --> eigan basis for A*A
 
 # Characteristic eqn for eigan values
+
+from copy import copy
+from itertools import permutations
+
+def permute(n):
+	# For pattern product:
+	# 	Each position in each row is the column
+	# 	Each elem in each row is the row
+	return list(permutations([i for i in range(n)]))
+
+
+v = permute(3)
+print(v)
+
+## Determinant of nxn matrix
+def det(A):
+	# let p be a pattern of the matrix A
+	# det = for all p in A --> sum( (sign of p)(prod p) )
+	# pattern:
+	#	the product of every element with every other element
+	#	except when row or col is shared with previous operand
+	# sign of p:
+	#	number of inversions from I in pattern
+	
+	representatives = permute(len(A[0]))
+	for index in range(len(A[0])):
+		pass		
 # Eigan vectors are in ker(A - dot(lambda, I))
 # Using Gram-Shmidt for orthonormalization
 # 	normalize v1 --> u1
